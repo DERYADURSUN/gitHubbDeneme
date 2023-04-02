eb57458572abca20bdb035076924fd5499738e60
DeryaDursun <derdursun55@gmail.com>
2023-04-02 22:46:59 +0300

    ikinci version

--- src/sunday_github/C01_gitHubb.java
+++ src/sunday_github/C01_gitHubb.java
@@ -3,5 +3,7 @@ package sunday_github;
 public class C01_gitHubb {
     public static void main(String[] args) {
         System.out.println("Github cok zoorrr");
+        System.out.println("selmmmm");
+        System.out.println("heygit");
     }
 }
